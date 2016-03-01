a78cb6b882a9b51e99ca2b7d0a7afeb873584747
Ilja Goushcha <iljagou@yahoo.com>
2016-03-01 12:04:56 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name profilePageTwoApp
+ * @description
+ * # profilePageTwoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('profilePageTwoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name profilePageTwoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the profilePageTwoApp
+ */
+angular.module('profilePageTwoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
