8cab1cd6e8b37a6ae0ea2056afab6b09acd80e60
Ilja Goushcha <iljagou@yahoo.com>
2016-03-06 21:34:52 -0500

    implemeted page numbering

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,38 @@ angular.module('profilePageTwoApp')
     
   	var vm = this;
 
+    vm.pageNumber = '/01';
+
+    vm.onEnterSectionOne = function() {
+      console.log('onEnterSectionOne');
+      vm.pageNumber = '/01';
+    };
+
+    vm.onEnterSectionTwo = function() {
+      console.log('onEnterSectionTwo');
+      vm.pageNumber = '/02';
+    };
+
+    vm.onEnterSectionThree = function() {
+      console.log('onEnterSectionThree');
+      vm.pageNumber = '/03';
+    };
+
+    vm.onEnterSectionFour = function() {
+      console.log('onEnterSectionFour');
+      vm.pageNumber = '/04';
+    };
+
+    vm.onEnterSectionFive = function() {
+      console.log('onEnterSectionFive');
+      vm.pageNumber = '/05';
+    };
+
+    vm.onEnterSectionSix = function() {
+      console.log('onEnterSectionSix');
+      vm.pageNumber = '/06';
+    };
+
   	vm.onLoad = function() {
   		console.log('inside onLoad() from MainCtrl');
   	};
